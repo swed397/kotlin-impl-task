ec9ea4f7befdc73e09eaaa9e5c80edd5bd8eb0d6
Rodion <swed197@mail.ru>
2023-03-14 02:10:36 +0500

    Hot fast fix

--- src/main/kotlin/com/example/demo/controllers/MovieController.kt
+++ src/main/kotlin/com/example/demo/controllers/MovieController.kt
@@ -4,14 +4,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("")
 class MovieController {
-
     private val logger = LoggerFactory.getLogger(javaClass)
 
 
